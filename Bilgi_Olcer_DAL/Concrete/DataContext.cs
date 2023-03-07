7fa5f9454dc537cbf927f30320e4572b433f82e7
Enes <enes431811@gmail.com>
2023-03-07 22:17:45 +0300

    Entity Framework has been added.

---
+++ Bilgi_Olcer_DAL/Concrete/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bilgi_Olcer_DAL.Concrete
+{
+    public class DataContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-UVMSNSF;Initial Catalog=Bilgi_Olcer;Integrated Security=True");
+        }
+        public DbSet<Exam> Exams { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+        public DbSet<Lesson> Lessons { get; set; }
+        public DbSet<Option> Options { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Test> Tests { get; set; }
+    }
+}
