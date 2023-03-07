b220d3696044822f3c2114a564994c6020ba9c3a
Enes <enes431811@gmail.com>
2023-03-07 22:29:44 +0300

    Project services and managers have been added in the Business Logic Layer.

---
+++ Bilgi_Olcer_BLL/Concrete/TestManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bilgi_Olcer_BLL.Concrete
+{
+    public class TestManager
+    {
+    }
+}
