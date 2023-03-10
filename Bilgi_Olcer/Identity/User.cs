d56098308bcffe5c6f7723d0759b6b4c7efe361e
Enes <enes431811@gmail.com>
2023-03-10 22:44:15 +0300

    Microsoft.AspNetCore.Identity.EntityFrameworkCore package has been added.

---
+++ Bilgi_Olcer/Identity/User.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Bilgi_Olcer.Identity
+{
+    public class User:IdentityUser
+    {
+    }
+}
---
+++ Bilgi_Olcer/Identity/UserIdentityDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bilgi_Olcer.Identity
+{
+    public class UserIdentityDbContext:IdentityDbContext<User>
+    {
+        public UserIdentityDbContext(DbContextOptions<UserIdentityDbContext>options):base(options)
+        {
+                
+        }
+    }
+}
