4a994f2bb0a8ed47eaef8dc45a8484fcb43b27ab
Enes <enes431811@gmail.com>
2023-03-14 19:26:41 +0300

    Login page has been revised.

--- Bilgi_Olcer/Controllers/UserController.cs
+++ Bilgi_Olcer/Controllers/UserController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bilgi_Olcer.Identity;
+using Bilgi_Olcer.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bilgi_Olcer.Controllers
 {
@@ -8,7 +10,17 @@ namespace Bilgi_Olcer.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Login(LoginModel entity)
+        {
+            return View();
+        }
         public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(User entity)
         {
             return View();
         }
---
+++ Bilgi_Olcer/Models/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bilgi_Olcer.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
