a111767a4249585d742510323a3e282280adeea6
Enes <enes431811@gmail.com>
2023-03-30 17:15:57 +0300

    Subject listing process will be continued.

--- Bilgi_Olcer/Controllers/ClassController.cs
+++ Bilgi_Olcer/Controllers/ClassController.cs
@@ -9,16 +9,25 @@ namespace Bilgi_Olcer.Controllers
         {
             List<ClassModel> classes = new List<ClassModel>()
             {
-                new ClassModel(){Name="Matematik"},
-                new ClassModel(){Name="Kimya"},
-                new ClassModel(){Name="Biyoloji"},
-                new ClassModel(){Name="Fizik"},
-                new ClassModel(){Name="Türk Edebiyatı"},
-                new ClassModel(){Name="Coğrafya"},
-                new ClassModel(){Name="Tarih"}
+                new ClassModel(){Id=1,Name="Matematik"},
+                new ClassModel(){Id=2,Name="Kimya"},
+                new ClassModel(){Id=3,Name="Biyoloji"},
+                new ClassModel(){Id=4,Name="Fizik"},
+                new ClassModel(){Id=5,Name="Türk Edebiyatı"},
+                new ClassModel(){Id=6,Name="Coğrafya"},
+                new ClassModel(){Id=7,Name="Tarih"},
+                new ClassModel(){Id=8,Name="Din Kültürü ve Ahlak Bilgisi"},
+                new ClassModel(){Id=9,Name="Felsefe"}
             };
             
             return View(classes);
+        }
+        public IActionResult Subject(ClassModel model)
+        {
+            //Id ile seçilen dersi yakalıyorum bundan sonra database üzerinden gidip ilgili ders hakkında konuları
+            // listelemem gereklidir.
+
+            return View(model.Name);
         }
     }
 }
