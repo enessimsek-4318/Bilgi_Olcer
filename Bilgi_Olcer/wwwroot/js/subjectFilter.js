d492a856beb16cd16a63cfaebc8a13cdce9cf192
Enes <enes431811@gmail.com>
2023-03-31 15:14:14 +0300

    Subject filtration has been completed with javascript. subjectFilter.js file has been included in the project.

---
+++ Bilgi_Olcer/wwwroot/js/subjectFilter.js
@@ -0,0 +1,26 @@
+﻿const derslerDiv = document.querySelector(".dersler")
+//console.log(derslerDiv)
+//----------------------
+//const dersler = document.querySelectorAll(".list-group-item");
+//console.log(dersler)
+//----------------------
+const konularget = document.querySelectorAll(".konular li")
+//console.log(konularget)
+//----------------------
+
+derslerDiv.addEventListener("click", (event) => {
+    //----------------------------------------------------
+    const clickedDers = event.target.innerText
+    //console.log(clickedDers)
+    //-----------------------
+    konularget.forEach((konu) => {
+        if (clickedDers == konu.className) {
+            konu.setAttribute("style", "display: block");
+        } else {
+            konu.setAttribute("style", "display: none");
+        }
+    });
+    //----------------------------------------------------
+})
+
+
