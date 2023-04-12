aec512299fca2f8eab3a44759724c80aaec635c5
Enes <enes431811@gmail.com>
2023-04-12 17:18:20 +0300

    Question page refreshing process has been completed.

--- Bilgi_Olcer/Controllers/ClassController.cs
+++ Bilgi_Olcer/Controllers/ClassController.cs
@@ -28,31 +28,35 @@ namespace Bilgi_Olcer.Controllers
             return View();
         }
         [HttpGet]
-        public IActionResult Test(string data)
+        public IActionResult Test(int? index,string data)
         {
 
-            // Soruları al
+            
             List<Question> questions = _questionService.GetQuestion(data);
 
-            // TestModel oluştur ve özellikleri ayarla
+           
             TestModel model = new TestModel();
             model.Questions = questions;
-            model.Index = 0;
+            if (index==null)
+            {
+                ViewBag.Index = 0;
+            }
+            else
+            {
+                ViewBag.Index = index;
+            }
+            
+            
 
-            // İlk soruyu görüntüle
+            
             return View(model);
+            
         }
         [HttpPost]
         public IActionResult Test(TestModel model)
         {
-            // Sonraki soruya gitmek için indeksi artır
-            model.Questions = _questionService.GetQuestion(model.Subject);
-            model.Index++;
 
-          
-
-            // Bir sonraki soruyu görüntüle
-            return View(model);
+            return RedirectToAction("Test",new {index=model.Index+1,data=model.Subject});
         }
 
     }
