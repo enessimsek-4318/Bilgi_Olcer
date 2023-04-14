13d51574e77ee25432b1c6199554245a906d64d6
Enes <enes431811@gmail.com>
2023-04-14 16:36:17 +0300

    Answer control process have been revised.

--- Bilgi_Olcer/Models/TestModel.cs
+++ Bilgi_Olcer/Models/TestModel.cs
@@ -7,5 +7,6 @@ namespace Bilgi_Olcer.Models
         public List<Question> Questions { get; set; } = new List<Question>();
         public int Index { get; set; }
         public string Subject { get; set; }
+        public string Option { get; set; }
     }
 }
