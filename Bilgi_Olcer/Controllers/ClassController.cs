03fd2a9a0f47e6557fbaad899bc3bf48c82c5dc3
Enes <enes431811@gmail.com>
2023-04-17 17:18:31 +0300

    Result page has been revised.

--- Bilgi_Olcer/Controllers/ClassController.cs
+++ Bilgi_Olcer/Controllers/ClassController.cs
@@ -2,6 +2,7 @@
 using Bilgi_Olcer.Models;
 using Bilgi_Olcer_BLL.Absract;
 using Entities;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq.Expressions;
@@ -48,50 +49,64 @@ namespace Bilgi_Olcer.Controllers
         [HttpPost]
         public IActionResult Test(TestModel model)
         {
-            int correct = 0;
+            // Varsayılan değerleri ayarlayın
+            //HttpContext.Session.SetInt32("correct", 0);
+            //HttpContext.Session.SetInt32("incorrect", 0);
+            //HttpContext.Session.SetInt32("empty", 0);
 
-            int incorrect = 0;
-
-            int empty = 0;
-            
             if (model.Option == model.Answer)
             {
-                correct++;
+                // Doğru cevap sayısını artırın
+                //var correct = HttpContext.Session.GetInt32("correct") ?? 0;
+                //HttpContext.Session.SetInt32("correct", correct + 1);
             }
             else if (model.Option == null)
             {
-                empty++;
+                // Boş cevap sayısını artırın
+                //var empty = HttpContext.Session.GetInt32("empty") ?? 0;
+                //HttpContext.Session.SetInt32("empty", empty + 1);
             }
             else
             {
-                incorrect++;
+                // Yanlış cevap sayısını artırın
+                //var incorrect = HttpContext.Session.GetInt32("incorrect") ?? 0;
+                //HttpContext.Session.SetInt32("incorrect", incorrect + 1);
             }
-            if (model.QuestionNumber<=model.Index+1)
-            {
-                return RedirectToAction("Result", new {correct=correct, incorrect=incorrect, empty=empty});
-                
-            }
-            return RedirectToAction("Test",new {index=model.Index+1,data=model.Subject});
-           
-             
-        }
-        [HttpGet]
-        public IActionResult Result(int correct, int incorrect, int empty)
-        {
-            if (correct==null || incorrect==null || empty==null)
-            {
-                // viewbag içerisinden gelen veri ekrana basılacak.
-            }
-            //var user = _userManager.FindByIdAsync(model.userId);
 
-          //  Name = User.Identity.Name;
-          return View();
+            if (model.QuestionNumber <= model.Index + 1)
+            {
+                // Test tamamlandıysa, sonuçları görüntülemek için Result metoduna yönlendirin
+                return RedirectToAction("Result");
+            }
+
+            // Bir sonraki soruya yönlendirin
+            return RedirectToAction("Test", new { index = model.Index + 1, data = model.Subject });
         }
+
+        [HttpGet]
+        public IActionResult Result()
+        {
+            // HttpContext.Session'dan doğru, yanlış ve boş cevap sayılarını alın
+            var correct = HttpContext.Session.GetInt32("correct") ?? 0;
+            var incorrect = HttpContext.Session.GetInt32("incorrect") ?? 0;
+            var empty = HttpContext.Session.GetInt32("empty") ?? 0;
+
+            ResultModel model = new ResultModel()
+            {
+                Correct = correct,
+                Incorrect = incorrect,
+                Empty = empty,
+                Name = User.Identity.Name
+            };
+            return View(model);
+        }
+
         [HttpPost]
         public IActionResult Result(int id)
         {
             return View();
         }
+
 
     }
 }
