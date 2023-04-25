d3c8cbe7f86ed114b85364dae0cc77390d0239c7
Enes <enes431811@gmail.com>
2023-04-25 18:45:48 +0300

    Windows Form app has been added in the project for admin CRUD process.

--- Bilgi_Olcer_WebAPI/Program.cs
+++ Bilgi_Olcer_WebAPI/Program.cs
@@ -11,8 +11,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddScoped<IAdminDal, AdminDal>();
 builder.Services.AddScoped<IAdminService, AdminManager>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ Bilgi_Olcer_WinForm/Form1.cs
@@ -0,0 +1,10 @@
+namespace Bilgi_Olcer_WinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
