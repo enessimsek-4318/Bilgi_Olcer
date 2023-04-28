324e10087d171928fc5804aa19ce3096fcbb5c93
Enes <enes431811@gmail.com>
2023-04-28 13:15:27 +0300

    The question list was displayed in winform with the created listview.

--- Bilgi_Olcer_WinForm/Form1.Designer.cs
+++ Bilgi_Olcer_WinForm/Form1.Designer.cs
@@ -28,21 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.question_list = new System.Windows.Forms.ListView();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.btn_list = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // listView1
+            // question_list
             // 
-            this.listView1.Location = new System.Drawing.Point(12, 51);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(776, 387);
-            this.listView1.TabIndex = 0;
-            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.question_list.Location = new System.Drawing.Point(12, 51);
+            this.question_list.Name = "question_list";
+            this.question_list.Size = new System.Drawing.Size(776, 387);
+            this.question_list.TabIndex = 0;
+            this.question_list.UseCompatibleStateImageBehavior = false;
+            this.question_list.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
             // 
             // comboBox1
             // 
@@ -87,17 +89,28 @@
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
             // 
+            // btn_list
+            // 
+            this.btn_list.Location = new System.Drawing.Point(672, 22);
+            this.btn_list.Name = "btn_list";
+            this.btn_list.Size = new System.Drawing.Size(75, 23);
+            this.btn_list.TabIndex = 6;
+            this.btn_list.Text = "Listele";
+            this.btn_list.UseVisualStyleBackColor = true;
+            this.btn_list.Click += new System.EventHandler(this.btn_list_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_list);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.question_list);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -106,11 +119,12 @@
 
         #endregion
 
-        private ListView listView1;
+        private ListView question_list;
         private ComboBox comboBox1;
         private ComboBox comboBox2;
         private Button button1;
         private Button button2;
         private Button button3;
+        private Button btn_list;
     }
 }
--- Bilgi_Olcer_WinForm/Form1.cs
+++ Bilgi_Olcer_WinForm/Form1.cs
@@ -1,3 +1,6 @@
+
+using Entities;
+
 namespace Bilgi_Olcer_WinForm
 {
     public partial class Form1 : Form
@@ -5,6 +8,25 @@ namespace Bilgi_Olcer_WinForm
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void btn_list_Click(object sender, EventArgs e)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:5045/");
+            HttpResponseMessage response = await client.GetAsync("api/Admin");
+            string result = await response.Content.ReadAsStringAsync();
+            List<Question> questions = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Question>>(result);
+            foreach (var item in questions)
+            {
+                
+            }
+
         }
     }
 }
