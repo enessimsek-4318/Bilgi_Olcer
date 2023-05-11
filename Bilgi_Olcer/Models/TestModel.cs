de7d1aefeb3a0c3aeb77195b86dfd5dd387504d2
Enes <enes431811@gmail.com>
2023-05-11 22:19:27 +0300

    The user results page has been created

--- Bilgi_Olcer/Models/TestModel.cs
+++ Bilgi_Olcer/Models/TestModel.cs
@@ -6,6 +6,7 @@ namespace Bilgi_Olcer.Models
     {
         public List<Question> Questions { get; set; } = new List<Question>();
         public int Index { get; set; }
+        public string Lesson { get; set; }
         public string Subject { get; set; }
         public string Option { get; set; }
         public string Answer { get; set; }
